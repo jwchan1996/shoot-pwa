240f84baa3c5a711c71ae398d694106954ace7f4
jwchan1996 <741755613@qq.com>
2019-03-28 16:53:47 +0800

    add load more

--- dist/index.js
+++ dist/index.js
@@ -7,9 +7,12 @@ var request = axios.create({
 var app = new Vue({
     el: '#app',
     data: {
-        postList: []
+        postList: [],
+        page: 1,
+        loading: true
     },
     mounted: function mounted() {
+        this.listenScrollToBottom();
         this.getRecommend();
     },
 
@@ -17,12 +20,11 @@ var app = new Vue({
         getRecommend: function getRecommend() {
             var _this = this;
 
-            request.get('/posts/both', {
+            request.get('/posts/type', {
                 params: {
                     status: 'public',
-                    type: 'tuijian',
                     page: 1,
-                    pageSize: 10
+                    pageSize: 20
                 }
             }).then(function (response) {
 
@@ -31,7 +33,7 @@ var app = new Vue({
                 if (response.data.code == 201) {
                     _this.postList = response.data.posts;
                 } else {
-                    console.log('没找到相关内容哦~');
+                    alert('没找到相关内容哦~');
                 }
             }).catch(function (error) {
                 console.log(error.data);
@@ -40,6 +42,55 @@ var app = new Vue({
         getImgSrc: function getImgSrc(content) {
             var m = content.match(/suo(.+?)\)/i);
             return m ? m[1].slice(2) : 'https://wx4.sinaimg.cn/mw690/0060lm7Tly1fvmtrka9p5j30b40b43yo.jpg';
+        },
+        listenScrollToBottom: function listenScrollToBottom() {
+            window.addEventListener('scroll', this.scrolltobottom, false);
+        },
+        scrolltobottom: function scrolltobottom() {
+            //变量scrollTop是滚动条滚动时，距离顶部的距离
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            //变量windowHeight是可视区的高度
+            var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+            //变量scrollHeight是滚动条的总高度
+            var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+            //滚动条到底部的条件
+            if (scrollTop + windowHeight == scrollHeight) {
+                console.log('到底了');
+                this.loadMore();
+            }
+        },
+        loadMore: function loadMore() {
+            var _this2 = this;
+
+            this.page++;
+            request.get('/posts/type', {
+                params: {
+                    status: 'public',
+                    page: this.page,
+                    pageSize: 20
+                }
+            }).then(function (response) {
+
+                console.log(response.data);
+
+                if (response.data.code === 201) {
+                    if (response.data.posts !== null) {
+                        for (var i = 0; i < response.data.posts.length; i++) {
+                            _this2.postList.push(response.data.posts[i]);
+                        }
+                        if (response.data.posts.length < 20) {
+                            _this2.loading = false;
+                        }
+                    } else {
+                        _this2.loading = false;
+                        //移除事件监听
+                        window.removeEventListener(_this2.scrolltobottom, false);
+                    }
+                }
+            }).catch(function (error) {
+                console.log(JSON.stringify(error));
+                _this2.loading = false;
+            });
         }
     }
 });
---
+++ index.js
@@ -0,0 +1,91 @@
+const request = axios.create({
+    baseURL: 'https://api.clicli.top'
+})
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        postList: [],
+        page: 1,
+        loading: true
+    },
+    mounted() {
+        this.listenScrollToBottom()
+        this.getRecommend()
+    },
+    methods: {
+        getRecommend() {
+            request.get('/posts/type', {
+                params: {
+                    status: 'public',
+                    page: 1,
+                    pageSize: 20
+                }
+            }).then(response => {
+
+                console.log(response.data)
+
+                if (response.data.code == 201) {
+                    this.postList = response.data.posts
+                } else {
+                    alert('没找到相关内容哦~')
+                }
+
+            }).catch(error => {
+                console.log(error.data)
+            })
+        },
+        getImgSrc(content) {
+            let m = content.match(/suo(.+?)\)/i)
+            return m ? m[1].slice(2) : 'https://wx4.sinaimg.cn/mw690/0060lm7Tly1fvmtrka9p5j30b40b43yo.jpg'
+        },
+        listenScrollToBottom() {
+            window.addEventListener('scroll',this.scrolltobottom,false)
+        },
+        scrolltobottom(){
+            //变量scrollTop是滚动条滚动时，距离顶部的距离
+            let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+            //变量windowHeight是可视区的高度
+            let windowHeight = document.documentElement.clientHeight || document.body.clientHeight
+            //变量scrollHeight是滚动条的总高度
+            let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+            //滚动条到底部的条件
+            if (scrollTop + windowHeight == scrollHeight) {
+                console.log('到底了')
+                this.loadMore()
+            }
+        },
+        loadMore() {
+            this.page ++
+            request.get('/posts/type', {
+                params: {
+                    status: 'public',
+                    page: this.page,
+                    pageSize: 20
+                }
+            }).then(response => {
+
+                console.log(response.data)
+
+                if (response.data.code === 201) {
+                    if(response.data.posts !== null){
+                        for ( let i = 0; i < response.data.posts.length; i ++ ) {
+                            this.postList.push(response.data.posts[i])
+                        }
+                        if(response.data.posts.length<20){
+                            this.loading = false
+                        }
+                    }else{
+                        this.loading = false
+                        //移除事件监听
+                        window.removeEventListener(this.scrolltobottom,false)
+                    }
+                }
+
+            }).catch(error => {
+                console.log(JSON.stringify(error))
+                this.loading = false
+            })
+        }
+    }
+})
