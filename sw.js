dd7a1c5e72c3f97ab6bd5f71eed18f1751cc7276
jwchan1996 <741755613@qq.com>
2019-03-28 20:11:53 +0800

    modify

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
 var cacheStorageKey = 'minimal-pwa-1'
 
-self.addEventListener('install', event => {  
-  event.waitUntil(
-    caches.open(cacheName)                   
-    .then(cache => cache.addAll([
-      './index.html',
-      './dist/index.js',            
-      './vendor/vue.min.js',
-      './vendor/axios.min.js'
-    ]))
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => cache.addAll([
+            '/',
+            './index.html',
+            './dist/index.js',
+            './vendor/vue.min.js',
+            './vendor/axios.min.js'
+        ]))
+    );
 });
 
 self.addEventListener('fetch', function (event) {
@@ -27,15 +28,14 @@ self.addEventListener('fetch', function (event) {
         })
     }
     event.respondWith(
-      caches.match(event.request)                    
-      .then(function (response) {
-        
-        if (response) {                              
-          return response;                           
-        }
-        
-        return fetch(event.request);                 
-      })
+        caches.match(event.request)
+        .then(function (response) {
+
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request);
+        })
     );
-  });
-  
+});
