a6bae1de9371ab6efc2c0f4d92ab18a47bb4fb25
jwchan1996 <741755613@qq.com>
2019-03-30 00:59:29 +0800

    test cache

--- sw.js
+++ sw.js
@@ -1,41 +1,43 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheName = 'minimal-pwa-01'
+var cacheStorageKey = 'minimal-pwa-01'
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName)
-        .then(cache => cache.addAll([
-            '/',
-            './index.html',
-            './dist/index.js',
-            './vendor/vue.min.js',
-            './vendor/axios.min.js'
-        ]))
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheStorageKey)
+            .then(cache => cache.addAll([
+                '/',
+                './index.html',
+                './dist/index.js',
+                './vendor/vue.min.js',
+                './vendor/axios.min.js'
+            ])).then(() => self.skipWaiting())
     );
-});
+})
 
-self.addEventListener('fetch', function (event) {
-    var request = event.request.url;
-    var api = /https:\/\/api.i-meto.com\/api\/v1/g;
-    // 动态缓存接口请求数据
-    if (api.test(request)) {
-        fetch(event.request).then(function (response) {
-            var key = event.request.url;
-            var value = response.clone();
-            caches.open(cacheName).then(function (cache) {
-                cache.put(key, value);
-            })
-        })
-    }
-    event.respondWith(
-        caches.match(event.request)
-        .then(function (response) {
-
-            if (response) {
-                return response;
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            if (response != null) {
+                return response
             }
-
-            return fetch(event.request);
+            return fetch(e.request.url)
         })
-    );
-});
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        //获取所有cache名称
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                // 获取所有不同于当前版本名称cache下的内容
+                cacheNames.filter(cacheNames => {
+                    return cacheNames !== cacheStorageKey
+                }).map(cacheNames => {
+                    return caches.delete(cacheNames)
+                })
+            )
+        }).then(() => {
+            return self.clients.claim()
+        })
+    )
+})
