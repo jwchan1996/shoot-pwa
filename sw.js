46d1e1cb5f072f0f5297e99351c5225b56061290
jwchan1996 <741755613@qq.com>
2019-03-30 11:24:56 +0800

    test add main

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheStorageKey = 'minimal-pwa01'
+var cacheStorageKey = 'minimal-pwa02'
 
 self.addEventListener('install', e => {
     e.waitUntil(
@@ -40,4 +40,14 @@ self.addEventListener('activate', function (e) {
             return self.clients.claim()
         })
     )
+})
+
+// 「弹出添加到主屏对话框」事件
+self.addEventListener('beforeinstallprompt', event => {
+    // 这个 `event.userChoice` 是一个 Promise ，在用户选择后 resolve
+    event.userChoice.then(result => {
+        console.log(result.outcome)
+        // 'accepted': 添加到主屏
+        // 'dismissed': 用户不想理你并向你扔了个取消
+    })
 })
